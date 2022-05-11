8cbf82b6f7770a78fd0aa37e29c4e6400a1c91fe
zetshiro <0xzetshiro@gmail.com>
2022-05-11 06:09:13 -0700

    fix: update the deploy strategy task

--- tasks/deploy-strategy.ts
+++ tasks/deploy-strategy.ts
@@ -3,9 +3,10 @@ import { task } from 'hardhat/config';
 task('deploy-strategy', 'Deploys a strategy')
   .addParam('strategy', 'The name of the strategy smart contract, e.g. JointStrategy')
   .addParam('vault', 'The address of the vault the strategy will interact with')
-  .setAction(async (args: { strategy: string; vault: string }, hh) => {
-    const strategyFactory = await hh.ethers.getContractFactory(args.strategy);
-    const strategy = await strategyFactory.deploy(args.vault);
+  .addOptionalParam('lendingPool', 'The lending pool the strategy integrates with')
+  .setAction(async ({ strategy: strategyName, vault, ...taskParams }: { strategy: string; vault: string; lendingPool?: string }, hh) => {
+    const strategyFactory = await hh.ethers.getContractFactory(strategyName);
+    const strategy = await strategyFactory.deploy(vault, ...Object.values(taskParams));
 
-    console.log(`Strategy ${args.strategy} deployed to ${strategy.address}.`);
+    console.log(`Strategy ${strategyName} deployed to ${strategy.address}.`);
   });
--- test/e2e/AaveLendingPoolV2Strategy.spec.ts
+++ test/e2e/AaveLendingPoolV2Strategy.spec.ts
@@ -1,1 +1,6 @@
-describe.skip('AaveLendingPoolV2Strategy', () => {});
+describe.skip('AaveLendingPoolV2Strategy', () => {
+  // test profitable harvest
+  // test migration
+  // test free funds
+  // test the trigger methods
+});
